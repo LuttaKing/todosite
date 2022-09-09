b6fa3ad8029794fff84bf2b9308cf2c4d1de0662
LuttaKing <denilsonwash@gmail.com>
2022-09-10 01:08:30 +0300

    fddfgd

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,10 @@ def index(request):
 
 def create_todo(request):
     form = TodoForm()
+    for field in form:
+        print(field)
+    
+    # form.fields[0].
     context = {'form':form}
     return render(request,'todo/create_todo.html', context)
 
