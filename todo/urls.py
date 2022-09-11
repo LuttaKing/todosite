962f83608f84408a021230f3a9ab7176a4e93400
LuttaKing <denilsonwash@gmail.com>
2022-09-12 00:01:18 +0300

    fjhgh

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.index, name='home'),
     path('create_todo',views.create_todo, name='create_todo'),
+     path('todo/<id>/',views.todo_detail, name='todo_detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,36 @@
 from django.shortcuts import render
 from .forms import TodoForm
+from django.views.decorators.csrf import csrf_exempt
+from .models import Todo
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
-    return render(request,'todo/index.html')
+    todos=Todo.objects.all()
+    context = {'todos':todos}
+    return render(request,'todo/index.html',context)
 
 def create_todo(request):
     form = TodoForm()
-    for field in form:
-        print(field)
-    
-    # form.fields[0].
     context = {'form':form}
+
+    if request.method == 'POST':
+        title = request.POST.get("Title")
+        description = request.POST.get("Description")
+        is_complete = request.POST.get("is_completed", False)
+        print(is_complete)
+
+        todo = Todo()
+        todo.title = title
+        todo.description = description
+        todo.is_completed = True if is_complete == 'on' else False
+        todo.save()
+
+        return HttpResponseRedirect(reverse("todo_detail",kwargs={'id':todo.pk}))
+
+
+
     return render(request,'todo/create_todo.html', context)
 
+def todo_detail(request,id):
+    return render(request,'todo/todo_detail.html',{})
