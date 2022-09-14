e1ae01cb68e20a2f61bc1bb31e4c8071bf1a85f1
LuttaKing <denilsonwash@gmail.com>
2022-09-14 20:51:04 +0300

    add models to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import User
-from django.contrib.auth.admin import UserAdmin
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=['username','email','is_staff']
+    search_fields=['username','email']
+
 
 admin.site.register(User,UserAdmin)
